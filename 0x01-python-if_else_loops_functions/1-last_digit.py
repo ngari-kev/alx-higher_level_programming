ebb0cfddd40c201c998725a7ace5df85f62f54ba
ngari-kev <kevngariwangui@gmail.com>
2023-10-31 23:32:41 +0300

    where does the number lie

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = number % 10
+if last > 5:
+    print("Last digit of %d is %d and is greater than 5" % (number, last))
+elif last == 0:
+    print("Last digit of %d is %d and is 0" % (number, last))
+else:
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
