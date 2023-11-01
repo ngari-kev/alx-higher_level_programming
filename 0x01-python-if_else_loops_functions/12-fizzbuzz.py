17467476058197bf669727d9e802bbb441808e5f
ngari-kev <kevngariwangui@gmail.com>
2023-11-01 03:40:08 +0300

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for _ in range(1, 101):
+        if _ % 3 == 0 and _ % 5 == 0:
+            print("{}".format("FizzBuzz"), end=" ")
+        elif _ % 3 == 0:
+            print("{}".format("Fizz"), end=" ")
+        elif _ % 5 == 0:
+            print("{}".format("Buzz"), end=" ")
+        else:
+            print("{}".format(_), end=" ")
