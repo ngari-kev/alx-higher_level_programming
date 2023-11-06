92be02bb1734bd5f22e21950b814e9d7600c4188
ngari-kev <kevngariwangui@gmail.com>
2023-11-06 18:21:01 +0300

    Item at index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if (len(my_list) - 1) < idx and idx > 0:
+    if idx > (len(my_list) - 1) or idx < 0:
         return None
     else:
         return my_list[idx]
