69ad7679ccdf2569c2c904928f58c43015a7c128
ngari-kev <kevngariwangui@gmail.com>
2023-11-06 23:37:35 +0300

    addong tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tup_1 = tuple(tuple_a[i] if i < len_a else 0 for i in range(0, 3))
-    tup_2 = tuple(tuple_b[i] if i < len_b else 0 for i in range(0, 3))
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    tup_1 = tuple(tuple_a[i] if i < len_a else 0 for i in range(0, 2))
+    tup_2 = tuple(tuple_b[i] if i < len_b else 0 for i in range(0, 2))
     result = tuple(tup_1[i] + tup_2[i] for i in range(max_len))
     return result
