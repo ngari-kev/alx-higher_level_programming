51ea27f26eb85379bc827d9eddb756a6563d0cfe
ngari-kev <kevngariwangui@gmail.com>
2023-11-06 23:29:39 +0300

    adding tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tup_1 = tuple(tuple_a[i] if i < len_a else 0 for i in range(0, 2))
-    tup_2 = tuple(tuple_b[i] if i < len_b else 0 for i in range(0, 2))
+    tup_1 = tuple(tuple_a[i] if i < len_a else 0 for i in range(0, 3))
+    tup_2 = tuple(tuple_b[i] if i < len_b else 0 for i in range(0, 3))
     result = tuple(tup_1[i] + tup_2[i] for i in range(max_len))
     return result
