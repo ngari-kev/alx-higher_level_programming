4e65336184a6c1c491d938f3da43f4785850d249
ngari-kev <kevngariwangui@gmail.com>
2023-11-14 22:51:07 +0300

    mul by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+A function that returns a dictionary with values * 2
+'''
+
+
+def multiply_by_2(a_dictionary):
+    dic = a_dictionary.copy()
+    for key in dic:
+        dic[key] *= 2
+    return dic
