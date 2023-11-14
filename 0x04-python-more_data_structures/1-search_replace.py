45de7bc52cae880372f9925986809cff2a6962b6
ngari-kev <kevngariwangui@gmail.com>
2023-11-14 10:18:00 +0300

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,18 @@
+'''
+   A function that searches for an element in a list and
+   replaces it with another.
+   my_list - is the initial list.
+   search  - is the element to replace in the list.
+   replace - is the new element.
+'''
+
+
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return None
+    new_list = my_list.copy()
+    while new_list is not None:
+        for _ in new_list:
+            if _ == search:
+                _ = replace
+    return new_list
