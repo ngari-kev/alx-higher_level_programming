7da3c4b5f372a54b1c1d311f702cd6520d0467df
ngari-kev <kevngariwangui@gmail.com>
2023-11-14 21:25:44 +0300

    update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,5 +5,5 @@ A function that updates a value in a dictionary.
 
 
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] =  value
+    a_dictionary[key] = value
     return a_dictionary
