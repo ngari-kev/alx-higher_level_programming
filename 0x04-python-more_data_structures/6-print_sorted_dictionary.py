ba07d1d050149c01b1519ba6b78b7718e2b48856
ngari-kev <kevngariwangui@gmail.com>
2023-11-14 11:32:45 +0300

    sorted dict

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+A function that returns a dictionary sorted in ascending order
+'''
+
+
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return None
+    sortd = dict(sorted(my_dict.items()))
+    return sortd
