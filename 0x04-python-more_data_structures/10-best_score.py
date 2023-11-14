a11c1bebf3605b58fcaad81952996271cfd7f3ae
ngari-kev <kevngariwangui@gmail.com>
2023-11-14 23:19:48 +0300

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,6 @@ A function that returns the best score
 
 
 def best_score(my_dict):
-   if my_dict is None:
+    if my_dict is None:
         return None
-   return max(my_dict, key=my_dict.get) if my_dict else None
-
+    return max(my_dict, key=my_dict.get) if my_dict else None
