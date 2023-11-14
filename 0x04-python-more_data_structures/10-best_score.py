836d7293bf4a2d320f4ef25702976f42952c001b
ngari-kev <kevngariwangui@gmail.com>
2023-11-14 23:16:58 +0300

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+A function that returns the best score
+'''
+
+
+def best_score(my_dict):
+   if my_dict is None:
+        return None
+   return max(my_dict, key=my_dict.get) if my_dict else None
+
