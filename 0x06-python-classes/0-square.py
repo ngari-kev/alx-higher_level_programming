1eec7319640bde9b6bab30b4e5b938c0cc0b6989
ngari-kev <kevngariwangui@gmail.com>
2023-11-26 19:57:17 +0300

    Square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+This module defines a square class
+'''
+
+
+class Square:
+    '''
+    This is an empty square class
+    '''
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This subdirectory contains solutions to the tasks under 0x06-python-classes.
