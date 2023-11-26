00ae88360cc7e699f784e47e65e05fe7c5fd96ff
ngari-kev <kevngariwangui@gmail.com>
2023-11-26 21:10:46 +0300

    # makes a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''
+This module defines a square class
+'''
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        '''new Square.
+        Args:
+            size (must be an int): The size of the new square.
+        '''
+        self.size = size
+
+    @property
+    def size(self):
+        '''access current size of the square'''
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        '''Sets the actual size of the square'''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        ''' This method calculates the area of a square'''
+        return (self.__size ** 2)
+
+    def my_print(self):
+        '''This method prints the square using the #'''
+        for col in range(self.__size):
+            for row in range(self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
