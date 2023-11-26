fab339fd9fa1025d8289dfa6e45aa5317a3d0797
ngari-kev <kevngariwangui@gmail.com>
2023-11-26 20:17:15 +0300

    init

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+This module defines a square class
+'''
+
+
+class Square:
+    '''
+    This is the initiaalization of the Square class
+    Args:
+        size - length of teh square
+    '''
+
+
+    def __init__(self, size):
+        self.__size = size
