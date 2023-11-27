ab779a6d195d8b0f6204c8e82561c105469f8a8f
ngari-kev <kevngariwangui@gmail.com>
2023-11-27 13:05:13 +0300

    The rectangle and its sides

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,34 +6,33 @@ class rectangle:
     '''The rectangle class'''
     def __init__(self, width=0, height=0):
         '''Initializing a new rectangle'''
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         '''Access the width parameter'''
         return (self.__width)
-    
+
     @property
     def height(self):
         '''Access the height parameter'''
         return (self.__height)
 
-
     @width.setter
     def width(self, value):
         '''Mutate or set the width parameter'''
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
         '''Mutate or set the height parameter'''
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
