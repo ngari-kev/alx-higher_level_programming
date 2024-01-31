53a9e428b10dadb42631660772da20b1abb73c63
ngari-kev <kevngariwangui@gmail.com>
2024-01-31 17:32:08 +0300

    rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    This is an empty rectangle class
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,4 @@
+0x08-python_more_classes
+========================
+
+Task: Create a Rectangle class and the various necessary aditions
