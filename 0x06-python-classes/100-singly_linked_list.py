fdee49a2e1069be418aef7945f72f1d0ef02eb52
ngari-kev <kevngariwangui@gmail.com>
2024-02-05 11:52:57 +0300

    Singly Linked List

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -12,24 +12,30 @@ class Node:
         next_node - pointer to the next node
     '''
     def __init__(self, data, next_node=None):
+        '''Initialize the data and next_node attributes'''
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        '''Getter method for the data attribute'''
         return self.__data
 
     @data.setter
     def data(self, value):
+        '''setter method for the data attribute'''
         self.__data = value
 
     @property
     def next_node(self):
+        '''Getter method for the next_node attribute'''
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        '''Setter method for the next_node attribute'''
         if not isinstance(value, Node) or if value is not None:
+            '''Ensure that next_node is a Node item or None'''
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
