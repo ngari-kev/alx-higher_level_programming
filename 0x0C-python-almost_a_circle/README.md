5a3859b592a6268b85ede52cf9fbd10c27f955a7
ngari-kev <kevngariwangui@gmail.com>
2024-02-15 21:48:05 +0300

    Update on rectangle module

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,1 @@
+This subdirectory contains solutions to tasks in the Python- almost a circle project.
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,6 +9,7 @@ class Rectangle(Base):
     """
     This defines a new Rectangle class that inherits from Base
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
@@ -22,18 +23,28 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        """WIdth setter"""
-        self.__width = value
+        """width setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
-        """Height setter"""
+        """height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Height setter"""
-        self.__height = value
+        """height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
@@ -48,9 +59,27 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ Setter for x"""
-        self.__x = value
+        if value < 0:
+            raise TypeError("x must be >= 0")
+        else:
+            self.__x = value
 
     @y.setter
     def y(self, value):
         """y setter"""
-        self.__y = value
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
+
+    def area(self):
+        """Calculate the area of the rectangle"""
+        return self.__height * self.__width
+
+    def display(self):
+        """prints the rectangle to stdout"""
+        for _ in range(self.height):
+            for _ in range(self.width):
+                print("#", end="")
+            print()
+
