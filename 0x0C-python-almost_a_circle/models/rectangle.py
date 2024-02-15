73967d039b706159a3a46a65cba4d6642a541fea
ngari-kev <kevngariwangui@gmail.com>
2024-02-15 22:41:29 +0300

    Positions considered

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,7 +79,11 @@ class Rectangle(Base):
 
     def display(self):
         """prints the rectangle to stdout"""
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
+            for _ in range(self.x):
+                print(" ", end="")
             for _ in range(self.width):
                 print("#", end="")
             print()
