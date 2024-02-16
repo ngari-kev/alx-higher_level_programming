09b73f9a1c152b61da26303db7949e16a68b7f6b
ngari-kev <kevngariwangui@gmail.com>
2024-02-16 23:25:51 +0300

    updated the Base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This module defines the base class"""
 
+
 class Base:
     """This is the base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         "Initializes the Base class"
         if id is None:
