17c79143c9313f1095761afbee8cf37a9a04a3c0
ngari-kev <kevngariwangui@gmail.com>
2024-02-22 21:09:50 +0300

    The square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,12 +5,12 @@
 from models.base import Base
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """This is the square class. It inherrits from Base"""
     def __init__(self, size, x=0, y=0, id=None):
         """Instantiation of a new square"""
         super().__init__(size, size, x, y, id)
-
 
     def __str__(self):
         """String representation of Square"""
