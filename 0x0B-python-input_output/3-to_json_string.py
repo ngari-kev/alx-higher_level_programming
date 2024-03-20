776a7237c1795ee9bd66bf0ea8f093b57f665375
ngari-kev <kevngariwangui@gmail.com>
2024-03-20 01:13:07 -0700

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+this module contains one function:to_json_string
+"""
+
+
+def to_json_string(my_obj):
+    """
+    converts an object to a JSON string
+
+    Args:
+        my_obj - object to be converted to JSON string
+
+    Returns:
+        JSON string
+    """
+    json_string = json.dumps(my_obj)
+    return json_string
