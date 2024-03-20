3c51143c6514f1542db1a77d2ec036974d40ad8f
ngari-kev <kevngariwangui@gmail.com>
2024-03-20 00:38:07 -0700

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """
+    This is a function that reads from a UTF-8 encoded file and 
+    prints to stdout.
+
+    Args:
+        filename - name of file.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                print(line, end='')
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+This is a subdirectoy that contains solutions to tasks on input/output
