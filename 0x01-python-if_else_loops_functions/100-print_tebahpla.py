f9c6f8ad4125cac869c6425d5782c96bb3f976c5
ngari-kev <kevngariwangui@gmail.com>
2024-03-21 22:28:18 +0300

    changed to executable

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for j in range(122, 96, -1):
-    print("{:c}".format(j if j % 2 == 0 else j - 32), end="")
+for k in range(122, 96, -1):
+    print("{:c}".format(k if k % 2 == 0 else k - 32), end="")
