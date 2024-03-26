43eae0baff1c23e334a3046c9804c41d58e18be9
ngari-kev <kevngariwangui@gmail.com>
2024-03-26 15:46:26 -0700

    Tempewqratures

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,6 @@
+-- displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+SELECT city, AVG(temp) AS avg_temp
+FROM temaparatures
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
