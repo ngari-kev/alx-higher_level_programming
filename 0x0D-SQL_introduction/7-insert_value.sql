e7e423e0fd006429e6c72eaae627978f17300a7b
ngari-kev <kevngariwangui@gmail.com>
2024-03-26 14:10:47 -0700

    inserting to the table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,3 @@
 -- a script that inserts a new row in the table first_table
-INSERT INTO first_table
-id = 89
-name = "Best School";
+INSERT INTO first_table (`id`, `name`)
+VALUES (89, "Best School");
