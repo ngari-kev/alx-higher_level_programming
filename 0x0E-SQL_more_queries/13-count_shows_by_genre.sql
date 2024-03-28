79071eb761540d76c0f8804ad4afa98283467136
ngari-kev <kevngariwangui@gmail.com>
2024-03-28 03:17:51 +0300

    13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,10 @@
 -- lists all shows contained in the database hbtn_0d_tvshows
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
--- If a show doesn’t have a genre, display NULL
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- If a show doesn’t have a genre, display NUL
+SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;L
