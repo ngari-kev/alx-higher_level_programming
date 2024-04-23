b590c7a1bcd311aa9f348be50845741167d962c1
ngari-kev <kevngariwangui@gmail.com>
2024-04-24 02:40:03 +0300

    multi line print

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let myStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const myStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++){
-  console.log(myStrings[i])
+  console.log(myStrings[i]);
 }
