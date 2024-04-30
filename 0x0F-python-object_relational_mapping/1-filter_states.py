ecd279cbf7397a271f35056dbe5bf706d7651725
ngari-kev <kevngariwangui@gmail.com>
2024-04-30 05:36:10 +0300

    Starts with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    con = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                          passwd=argv[2], db=argv[3], charset='utf8')
+
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    entries = cursor.fetchall()
+
+    for entry in entries:
+        if entry[1].startswith('N'):
+            print(entry)
+
+    cursor.close()
+    con.close()
